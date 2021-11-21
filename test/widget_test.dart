25476795f576253511bbe3fcb159b8475d0cf7be
kuldii <sandikha.rahardi@gmail.com>
2021-11-21 22:38:52 +0700

    Penilaian Jawaban

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,44 +1,35 @@
+import 'package:stats/stats.dart';
+
 void main() {
-  List dataAngka = [
-    3,
-    18,
-    45,
-    16,
-    2,
-    5,
-    7,
-    9,
-    10,
-    1,
-    16,
-    11,
-    4,
-    8,
-    11,
-    16,
-    32,
-    56
-  ];
+  List<int> dataAngka = [3, 18, 45, 16, 2, 5, 7, 9, 10, 1, 16, 11, 4, 8, 11, 16, 32, 56];
   int index = 0;
   int maxData = dataAngka.length;
 
+  // sudah bagus memanfaatkan fungsi sort
   //mengurutkan ke terbesar
   dataAngka.sort();
   print(dataAngka);
 
-  dataAngka.sort((a, b) => a.compareTo(b));
-  print(dataAngka);
+  // tidak diperlukan
+  // dataAngka.sort((a, b) => a.compareTo(b));
+  // print(dataAngka);
 
+  // langsung buat ke list saja
   //mengurutkan ke terkecil
-  final reverseValue = dataAngka.reversed;
+  final reverseValue = dataAngka.reversed.toList();
   print(reverseValue);
-  dataAngka.sort((b, a) => a.compareTo(b));
-  print(dataAngka);
 
+  // tidak diperlukan
+  // dataAngka.sort((b, a) => a.compareTo(b));
+  // print(dataAngka);
+
+  // bisa pakai package stats
   //MEAN
-  final mean = dataAngka.reduce((a, b) => a + b) / dataAngka.length;
-  print(mean);
+  // final mean = dataAngka.reduce((a, b) => a + b) / dataAngka.length;
+  // print(mean);
+  print(Stats.fromData(dataAngka).average);
 
+  // ini blm sampai jawabannya
   // MODUS
   var map = Map();
 
@@ -50,20 +41,64 @@ void main() {
     }
   });
 
-  print(map);
+  // jangan langsung di print butuh tambahan step seperti berikut
+  // print(map);
 
-  //MEDIAN
-  final median;
+  List keyData = map.keys.toList();
+  List valueData = map.values.toList();
 
-  int middle = dataAngka.length ~/ 2;
+  print("\nExtract Data Map");
+  print("Nilai : $keyData");
+  print("Banyak : $valueData");
+  print("\n--------------------------\nKESIMPULAN\n");
 
-  if (dataAngka.length % 2 == 1) {
-    median = dataAngka[middle];
-  } else {
-    median = ((dataAngka[middle - 1] + dataAngka[middle]) / 2.0).round();
+  int maksimalData = valueData[0];
+
+  for (var i = 0; i < valueData.length; i++) {
+    if (valueData[i] > maksimalData) {
+      maksimalData = valueData[i];
+    }
   }
 
-  return print(median);
+  int totalMaksimalData = 0;
+  for (var item in valueData) {
+    if (item == maksimalData) {
+      totalMaksimalData++;
+    }
+  }
+
+  maksimalData = valueData[0];
+  int indexMaksimalData = 0;
+
+  if (totalMaksimalData != 1) {
+    print("TIDAK ADA MODUS");
+  } else {
+    for (var i = 0; i < valueData.length; i++) {
+      if (valueData[i] > maksimalData) {
+        maksimalData = valueData[i];
+        indexMaksimalData = i;
+      }
+    }
+
+    print(
+      "Nilai yang sering muncul : ${keyData[indexMaksimalData]}\nSebanyak : ${valueData[indexMaksimalData]} x",
+    );
+  }
+
+  // bisa pakai package stats
+  //MEDIAN
+  // final median;
+
+  // int middle = dataAngka.length ~/ 2;
+
+  // if (dataAngka.length % 2 == 1) {
+  //   median = dataAngka[middle];
+  // } else {
+  //   median = ((dataAngka[middle - 1] + dataAngka[middle]) / 2.0).round();
+  // }
+
+  // return print(median);
+  print(Stats.fromData(dataAngka).median);
 }
     
 
